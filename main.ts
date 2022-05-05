1f7832cf2afabdec6704bd3076af6cfb5f64180d
msoruc <msoruc@gmail.com>
2022-05-05 13:11:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 hummingbird.startHummingbird()
 basic.forever(function () {
-    hummingbird.setPositionServo(FourPort.One, input.rotation(Rotation.Pitch))
-    hummingbird.setPositionServo(FourPort.Two, input.rotation(Rotation.Pitch))
+    hummingbird.setPositionServo(FourPort.One, input.acceleration(Dimension.X) / 10)
+    hummingbird.setPositionServo(FourPort.Two, input.acceleration(Dimension.Y) / 10)
 })
